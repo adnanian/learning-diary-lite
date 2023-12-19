346ec067852f5c4ecc121e9754128cb939dc84f5
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-12-18 22:41:07 -0600

    Display entries

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
+import { useEffect, useState } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:3000/entries')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setEntries(data);
+      });
+  }, []);
+
+
+  const [entries, setEntries] = useState([]);
 
   return (
     <>
       <header>
         <NavBar />
       </header>
-      <Outlet />
+      <Outlet context={entries}/>
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,33 @@
-//import { useState, useEffect} from "react";
+import { useState, useEffect} from "react";
 import "../page-stylesheets/Home.css";
 
 function Home() {
+    useEffect(() => {
+        fetch('http://localhost:3000/entries')
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            setEntries(data);
+          });
+      }, []);
+    
+    
+      const [entries, setEntries] = useState([]);
+
+      const entryList = entries.map((entry) => {
+        return (
+            <option key={entry.id} value={entry.title}>{entry.title}</option>
+        )
+      });
 
     return (
         <main>
             <h1 id="welcome-banner">Welcome to Learning Diary Lite! Let's get back to journaling our learning! :D</h1>
+            <div>
+                <select>
+                    {entryList}
+                </select>
+            </div>
         </main>
     );
 }
