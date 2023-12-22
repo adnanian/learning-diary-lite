69b9746731efff8a2284dcf01b659bfb51e6ae3d
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-12-22 11:18:52 -0600

    Complete LearningWishList

--- src/pages/LearningWishList.js
+++ src/pages/LearningWishList.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "../page-stylesheets/LearningWishList.css";
 
 function LearningWishList() {
     useEffect(() => {
@@ -12,7 +13,7 @@ function LearningWishList() {
     const [wishlist, setWishList] = useState([]);
     const [savedTitle, setSavedTitle] = useState("");
 
-    const listItems = wishlist.map((item) => <li>{item.savedTitle}</li>);
+    const listItems = wishlist.map((item) => <li key={item.id} className="item">{item.savedTitle}</li>);
 
     function addSavedTitle(event) {
         event.preventDefault();
@@ -33,21 +34,23 @@ function LearningWishList() {
     }
 
     return (
-        <main>
-            <h1>List of What I Want to Learn in the Future</h1>
-            <form onSubmit={addSavedTitle}>
-                <h2>Save something you would like to learn in the future.</h2>
-                <label htmlFor="savedTitle">Title</label>
-                <input
-                    id="savedTitle"
-                    name="savedTitle"
-                    type="text"
-                    value={savedTitle}
-                    onChange={(event) => setSavedTitle(event.target.value)}
-                />
-                <input type="submit" />
+        <main id="wishlist-page">
+            <h1 className="wishlist-element">List of What I Want to Learn in the Future</h1>
+            <form id="wishlist-form" onSubmit={addSavedTitle}>
+                <h2 className="wishlist-element">Save something you would like to learn in the future.</h2>
+                <div id="input-div">
+                    <label htmlFor="savedTitle">Title</label>
+                    <input
+                        id="savedTitle"
+                        name="savedTitle"
+                        type="text"
+                        value={savedTitle}
+                        onChange={(event) => setSavedTitle(event.target.value)}
+                    />
+                    <input id="submit-saved-title" type="submit" />
+                </div>
             </form>
-            <ul>
+            <ul id="wishlist" className="wishlist-element">
                 {listItems}
             </ul>
         </main>
