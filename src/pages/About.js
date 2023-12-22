87fb77148b2165f08376a7f51d6e48153173e95c
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-12-22 10:39:05 -0600

    Complete Abou.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,16 @@
+import "../page-stylesheets/About.css";
+
 function About() {
     return (
-        <div>
-            <h1 className="to-be-added">To be added</h1>
+        <div id="about-section">
+            <p id="about-text">
+                Learning Diary Lite is a web application that allows users to write educational
+                and beneficial content that they consume. It serves as a space for users to
+                explore material that they find interesting to learn (and more importantly,
+                matter much more in life, than the nonsense that is shoved down our throats
+                in the modern education system). With this handy tool, you never have to
+                buy another notebook again.
+            </p>
         </div>
     )
 }
