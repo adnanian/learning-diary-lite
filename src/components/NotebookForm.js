8e7248f8a04ba3ee624790924a1696f533e15900
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-12-22 07:00:57 -0600

    Complete NotebookForm.js

--- src/components/NotebookForm.js
+++ src/components/NotebookForm.js
@@ -1,4 +1,4 @@
-function NotebookForm() {
+function NotebookForm( {onAddNotebook} ) {
     const [formData, setFormData] = useState(
         {
             title: "",
@@ -12,13 +12,46 @@ function NotebookForm() {
     function handleChange(event) {
         setFormData({
             ...formData,
-            
+            [event.target.name]: event.target.value
         });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        fetch ('http://localhost:3001/notebooks', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+            .then((response) => response.json())
+            .then((data) => onAddNotebook(data));
     }
 
     return (
         <form>
-            
+            <div>
+                <label htmlFor="title">Title</label>
+                <input id="title" name="title" type="text" value={title} onChange={handleChange}/>
+            </div>
+            <div>
+                <label htmlFor="author">Author</label>
+                <input id="author" name="author" type="text" value={author} onChange={handleChange}/>
+            </div>
+            <div>
+                <label htmlFor="year">Year</label>
+                <input id="year" name="year" type="text" value={year} onChange={handleChange}/>
+            </div>
+            <div>
+                <label htmlFor="type">Type</label>
+                <input id="type" name="type" type="text" value={type} onChange={handleChange}/>
+            </div>
+            <div>
+                <label htmlFor="length">Length</label>
+                <input id="length" name="length" type="text" value={length} onChange={handleChange}/>
+            </div>
+            <input type="submit" />
         </form>
     );
 }
